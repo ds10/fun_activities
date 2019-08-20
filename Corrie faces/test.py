bc2a68c4795691fcbac3280b0743d28a33db0677
David Sherlock <davidsherlock163@hotmail.com>
2019-08-20 12:14:03 +0100

    add liz, catchy and some randomer

--- Corrie faces/test.py
+++ Corrie faces/test.py
@@ -16,14 +16,22 @@ frame_height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 output_video = cv2.VideoWriter('output.mp4', codec, fps, (frame_width,frame_height))
 
 # Load some sample pictures and learn how to recognize them.
-female_image = face_recognition.load_image_file("emma.jpeg")
-female_face_encoding = face_recognition.face_encodings(female_image)[0]
+emma = face_recognition.load_image_file("emma.jpeg")
+emma = face_recognition.face_encodings(emma)[0]
 
-male_image = face_recognition.load_image_file("steve.jpeg")
+liz = face_recognition.load_image_file("liz.JPG")
+liz = face_recognition.face_encodings(liz)[0]
+
+cathy = face_recognition.load_image_file("Cathy.JPG")
+cathy = face_recognition.face_encodings(cathy)[0]
+
+male_image = face_recognition.load_image_file("steve.jpg")
 male_face_encoding = face_recognition.face_encodings(male_image)[0]
 
 known_faces = [
-    female_face_encoding,
+    emma,
+    liz,
+    cathy,
     male_face_encoding
 ]
 
@@ -31,8 +39,9 @@ known_faces = [
 face_locations = []
 face_encodings = []
 face_names = []
-frame_number = 0
 
+frame_number = 0
+counts = [0,0,0,0]
 print 
 while True:
     # Grab a single frame of video
@@ -51,6 +60,7 @@ while True:
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
     face_names = []
+    
     for face_encoding in face_encodings:
         # See if the face is a match for the known face(s)
         match = face_recognition.compare_faces(known_faces, face_encoding, tolerance=0.50)
@@ -60,8 +70,28 @@ while True:
         name = None
         if match[0]:
             name = "emma"
+            frameNo = counts[0]
+            counts[0] = counts[0] + 1
+            text = name + ": " + str(frameNo)
+
         elif match[1]:
+            name = "liz"
+            frameNo = counts[1]
+            counts[1] = counts[1] + 1
+            text = name + ": " + str(frameNo)
+
+        elif match[2]:
+            name = "cathy"
+            frameNo = counts[2]
+            counts[2] = counts[2] + 1
+            text = name + ": " + str(frameNo)
+
+        elif match[3]:
             name = "steve"
+            frameNo = counts[3]
+            counts[3] = counts[3] + 1
+            text = name + ": " + str(frameNo)
+        
 
         face_names.append(name)
 
@@ -74,9 +104,10 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
         # Draw a label with a name below the face
+        
         cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+        cv2.putText(frame, text, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
 
     # Write the resulting image to the output video file
     print("Writing frame {} / {}".format(frame_number, length))
